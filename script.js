7aa22676b640a60c7d348d2a45de9bca47d35546
melmollie <43340481+melmollie@users.noreply.github.com>
2018-10-30 13:32:04 +0200

    Add input field and button
    Added input field, where you can write value 5-10. If you write something else it'll show you custom error message.

--- script.js
+++ script.js
@@ -212,3 +212,20 @@ function ageFunction() {
      }
      document.getElementById("ageresult").innerHTML = drinkable;
 }
+
+function numberInputFunction() {
+    let message, x;
+    message = document.getElementById("p01");
+    message.innerHTML = ""; 
+    x = document.getElementById("numberdemo").value; 
+    try {
+        if (x == "") throw "empty";
+        if (isNaN(x)) throw "not a number";
+        x = Number(x);
+        if(x < 5) throw "too low";
+        if(x > 10) throw "too high";
+    }
+    catch(err) {
+        message.innerHTML = "Input is " + err; 
+    }
+}
